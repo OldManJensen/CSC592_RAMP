5dea4f332291ebfaf1b88c369515974df1446dc7
OldManJensen <98491702+OldManJensen@users.noreply.github.com>
2025-04-26 12:47:01 -0400

    Update RAMP_scratch_cifar10.sh
    At first, I just changed the learning rate, but then I decided to really be thorough. I changed lr-schedule to step and gave the bash script epoch milestones to decide when the learning rate should be lowered. The LR will be multiplied by 0.1 once it hits each milestone.  
    
    Number of epochs increased.

--- scripts/cifar10/RAMP_scratch_cifar10.sh
+++ scripts/cifar10/RAMP_scratch_cifar10.sh
@@ -2,10 +2,10 @@
 # pretrain on wideresnet
 for seed in 0
 do
-    CUDA_VISIBLE_DEVICES='0' python RAMP_wide_resnet.py --lr-max 0.1  --lr-schedule=superconverge --at_iter 10 --epochs 30 --save_freq 10 --eval_freq 10 --fname RAMP_beta_0.5_lbd_2_wide_trades_$seed --kl --max --final_eval --gp --lbd 2 --seed $seed --wide
+    CUDA_VISIBLE_DEVICES='0' python RAMP_wide_resnet.py --lr-max 0.1  --lr-schedule=step --lr-milestone 50 70 --lr-gamma 0.1 --at_iter 10 --epochs 100 --save_freq 20 --eval_freq 20 --fname RAMP_beta_0.5_lbd_2_wide_trades_$seed --kl --max --final_eval --gp --lbd 2 --seed $seed --wide
 done
 
 for seed in 0 1 2 3 4
 do
-    CUDA_VISIBLE_DEVICES='0' python RAMP.py --lr-max 0.05  --lr-schedule=static --at_iter 10 --epochs 80 --save_freq 10 --eval_freq 10 --fname RAMP_beta_0.5_lbd_5_$seed --kl --max --final_eval --gp --lbd 5 --seed $seed
+    CUDA_VISIBLE_DEVICES='0' python RAMP.py --lr-max 0.05  --lr-schedule=step --lr-milestone 100 150 180 --at_iter 10 --epochs 200 --save_freq 20 --eval_freq 20 --fname RAMP_beta_0.5_lbd_5_$seed --kl --max --final_eval --gp --lbd 5 --seed $seed
 done
