c3d7dd9ecc9765eccacde5859af558a34bcd6646
OldManJensen <98491702+OldManJensen@users.noreply.github.com>
2025-04-26 12:52:14 -0400

    Update RAMP_scratch_cifar10.sh

--- scripts/cifar10/RAMP_scratch_cifar10.sh
+++ scripts/cifar10/RAMP_scratch_cifar10.sh
@@ -7,5 +7,5 @@ done
 
 for seed in 0 1 2 3 4
 do
-    CUDA_VISIBLE_DEVICES='0' python RAMP.py --lr-max 0.05  --lr-schedule=step --lr-milestone 100 150 180 --at_iter 10 --epochs 200 --save_freq 20 --eval_freq 20 --fname RAMP_beta_0.5_lbd_5_$seed --kl --max --final_eval --gp --lbd 5 --seed $seed
+    CUDA_VISIBLE_DEVICES='0' python RAMP.py --lr-max 0.05  --lr-schedule=step --lr-milestone 100 150 180 --lr-gamma 0.1 --at_iter 10 --epochs 200 --save_freq 20 --eval_freq 20 --fname RAMP_beta_0.5_lbd_5_$seed --kl --max --final_eval --gp --lbd 5 --seed $seed
 done
